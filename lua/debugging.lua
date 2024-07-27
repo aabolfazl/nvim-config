1306c7b5f31666c850813602ceb2adef957978e8
Ariyan Eghbal <ryn.eghbal@gmail.com>
2024-07-27 22:00:04 +0300

    add support for vscode launch.json+others
    Add support for launch.json
    Add debug support
    Add support for python general launch
    Add some key bindings

--- lua/debugging.lua
+++ lua/debugging.lua
@@ -164,17 +164,18 @@ end
 dap.listeners.before.launch.dapui_config = function()
   dapui.open()
 end
-dap.listeners.before.event_terminated.dapui_config = function()
-  dapui.close()
-end
-dap.listeners.before.event_exited.dapui_config = function()
-  dapui.close()
-end
+--dap.listeners.before.event_terminated.dapui_config = function()
+--  dapui.close()
+--end
+--dap.listeners.before.event_exited.dapui_config = function()
+--  dapui.close()
+--end
 vim.keymap.set('n', '<leader>dt', dap.toggle_breakpoint, {})
 vim.keymap.set('n', '<leader>dc', dap.continue, {})
 vim.keymap.set('n', '<leader>de', dapui.eval, {})
 
 vim.keymap.set('n', '<F5>', dap.continue , {})
+vim.keymap.set('n', '<leader>dc', dapui.close , {})
 vim.keymap.set('n', '<S-F5>', dap.terminate , {})
 vim.keymap.set('n', '<F10>', dap.step_over , {})
 vim.keymap.set('n', '<F11>', dap.step_into , {})
@@ -197,4 +198,256 @@ vim.keymap.set('n', '<Leader>ds', function()
   local widgets = require('dap.ui.widgets')
   widgets.centered_float(widgets.scopes)
 end)
+
+local function is_virtualenv_active()
+  return vim.env.VIRTUAL_ENV ~= nil and vim.env.VIRTUAL_ENV ~= ""
+end
+
+function venv_bin_detection(tool)
+    if is_virtualenv_active() then 
+        return vim.env.VIRTUAL_ENV .. '/bin/python'
+    else
+        local cwd = vim.loop.cwd()
+        if vim.fn.executable(cwd .. '/.venv/bin/' .. tool) == 1 then
+            return cwd .. '/.venv/bin/' .. tool
+        end
+        return tool
+    end
+end
+
+function venv_python_path()
+    local cwd = vim.loop.cwd()
+    local where = venv_bin_detection('python')
+    if where == 'python' then
+        -- local handle = io.popen("which python3")
+        -- local result = handle:read("*a")
+        -- handle:close()
+        -- print(result)
+        -- return result
+        return '/usr/bin/python3'
+    end
+    return where
+end
+
+
+--    require('dap-python').resolve_python = function()
+--        return venv_python_path()
+--    end
+dap.configurations.python = {
+    {
+        type = 'python';
+        request = 'launch';
+        name = "Launch file";
+        program = "${file}";
+--        pythonPath = venv_python_path()
+    },
+    {
+        type = 'debugpy',
+        request = 'launch',
+        name = 'Django',
+        program = '${workspaceFolder}/manage.py',
+        args = {
+            'runserver',
+        },
+        justMyCode = true,
+        django = true,
+        console = "integratedTerminal",
+--        pythonPath = venv_python_path()
+    },
+    {
+        type = 'python';
+        request = 'attach';
+        name = 'Attach remote';
+        connect = function()
+            return {
+                host = 'localhost',
+                port = 5678
+            }
+        end;
+    },
+    {
+        type = 'python';
+        request = 'launch';
+        name = 'Launch file with arguments';
+        program = '${file}';
+        args = function()
+            local args_string = vim.fn.input('Arguments: ')
+            return vim.split(args_string, " +")
+        end;
+        console = "integratedTerminal",
+--        pythonPath = venv_python_path()
+    }
+}
+
+
+dap.listeners.on_config['debugpy'] = function(config)
+    local final_config = vim.deepcopy(config)
+  
+    -- Placeholder expansion for launch directives
+    local placeholders = {
+      ["${file}"] = function(_)
+        return vim.fn.expand("%:p")
+      end,
+      ["${fileBasename}"] = function(_)
+        return vim.fn.expand("%:t")
+      end,
+      ["${fileBasenameNoExtension}"] = function(_)
+        return vim.fn.fnamemodify(vim.fn.expand("%:t"), ":r")
+      end,
+      ["${fileDirname}"] = function(_)
+        return vim.fn.expand("%:p:h")
+      end,
+      ["${fileExtname}"] = function(_)
+        return vim.fn.expand("%:e")
+      end,
+      ["${relativeFile}"] = function(_)
+        return vim.fn.expand("%:.")
+      end,
+      ["${relativeFileDirname}"] = function(_)
+        return vim.fn.fnamemodify(vim.fn.expand("%:.:h"), ":r")
+      end,
+      ["${workspaceFolder}"] = function(_)
+        return vim.fn.getcwd()
+      end,
+      ["${workspaceFolderBasename}"] = function(_)
+        return vim.fn.fnamemodify(vim.fn.getcwd(), ":t")
+      end,
+      ["${env:([%w_]+)}"] = function(match)
+        return os.getenv(match) or ""
+      end,
+    }
+    local expand_vars = function(v)
+      local patterns = {
+          ["${([%w_]+)}"] = function(match)
+              return final_config.env[match] or os.getenv(match) or ""
+          end,
+      }
+      for p, fn in pairs(patterns) do 
+          v = v:gsub(p, fn)
+      end 
+
+      return v 
+    end 
+    if final_config.envFile then
+      local filePaths = final_config.envFile
+      if type(filePaths) == "string" then
+          filePaths = {filePaths}
+      end
+      for index,filePath in ipairs(filePaths) do
+          for key, fn in pairs(placeholders) do
+            filePath = filePath:gsub(key, fn)
+          end
+      
+          for line in io.lines(filePath) do
+            if not (string.match(line, "^[%s]*#") or string.match(line, "^[%s]*$")) then
+                local words = {}
+                for word in string.gmatch(line, "[^=]+") do
+                  table.insert(words, word)
+                end
+                if not final_config.env then
+                  final_config.env = {}
+                end
+                words[2] = expand_vars(words[2])
+                --print(vim.inspect(words))
+                final_config.env[words[1]] = words[2]
+            end
+          end
+      end
+    end
+    return final_config
+end
+
+
+
+--dap.adapters.debugpy = {
+--    type = 'executable',
+--    command = venv_python_path(),
+--    args = {'-m', 'debugpy.adapter'},
+--    enrich_config = function(finalConfig, on_config)
+--      local final_config = vim.deepcopy(finalConfig)
+--    
+--      -- Placeholder expansion for launch directives
+--      local placeholders = {
+--        ["${file}"] = function(_)
+--          return vim.fn.expand("%:p")
+--        end,
+--        ["${fileBasename}"] = function(_)
+--          return vim.fn.expand("%:t")
+--        end,
+--        ["${fileBasenameNoExtension}"] = function(_)
+--          return vim.fn.fnamemodify(vim.fn.expand("%:t"), ":r")
+--        end,
+--        ["${fileDirname}"] = function(_)
+--          return vim.fn.expand("%:p:h")
+--        end,
+--        ["${fileExtname}"] = function(_)
+--          return vim.fn.expand("%:e")
+--        end,
+--        ["${relativeFile}"] = function(_)
+--          return vim.fn.expand("%:.")
+--        end,
+--        ["${relativeFileDirname}"] = function(_)
+--          return vim.fn.fnamemodify(vim.fn.expand("%:.:h"), ":r")
+--        end,
+--        ["${workspaceFolder}"] = function(_)
+--          return vim.fn.getcwd()
+--        end,
+--        ["${workspaceFolderBasename}"] = function(_)
+--          return vim.fn.fnamemodify(vim.fn.getcwd(), ":t")
+--        end,
+--        ["${env:([%w_]+)}"] = function(match)
+--          return os.getenv(match) or ""
+--        end,
+--      }
+--      local expand_vars = function(v)
+--        local patterns = {
+--            ["${([%w_]+)}"] = function(match)
+--                return final_config.env[match] or os.getenv(match) or ""
+--            end,
+--        }
+--        for p, fn in pairs(patterns) do 
+--            v = v:gsub(p, fn)
+--        end 
+--
+--        return v 
+--      end 
+--      if final_config.envFile then
+--        local filePaths = final_config.envFile
+--        if type(filePaths) == "string" then
+--            filePaths = {filePaths}
+--        end
+--        for index,filePath in ipairs(filePaths) do
+--            for key, fn in pairs(placeholders) do
+--              filePath = filePath:gsub(key, fn)
+--            end
+--        
+--            for line in io.lines(filePath) do
+--              if not (string.match(line, "^[%s]*#") or string.match(line, "^[%s]*$")) then
+--                  local words = {}
+--                  for word in string.gmatch(line, "[^=]+") do
+--                    table.insert(words, word)
+--                  end
+--                  if not final_config.env then
+--                    final_config.env = {}
+--                  end
+--                  words[2] = expand_vars(words[2])
+--                  --print(vim.inspect(words))
+--                  final_config.env[words[1]] = words[2]
+--              end
+--            end
+--        end
+--      end
+--    
+--      on_config(final_config)
+--    end,
+--}
+
+
+
+
+
+--set_python_dap()
+--vim.api.nvim_create_autocmd({"DirChanged", "BufEnter"}, {
+--    callback = function() set_python_dap() end,
+--})
 
