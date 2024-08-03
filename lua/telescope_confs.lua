023631dfadc7ff0c295bdaa10a3bd7788d26c45b
Abolfazl Abbasi <aabolfazlit@gmail.com>
2024-08-03 18:45:32 +0300

    Update Telescope fg command

--- lua/telescope_confs.lua
+++ lua/telescope_confs.lua
@@ -1,26 +1,32 @@
-require("telescope").setup {
-  extensions = {
-    file_browser = {
-      theme = "ivy",
-      -- disables netrw and use telescope-file-browser in its place
-      hijack_netrw = true,
-      mappings = {
-        ["i"] = {
-          -- your custom insert mode mappings
-        },
-        ["n"] = {
-          -- your custom normal mode mappings
-        },
+require('telescope').setup {
+  defaults = {
+    vimgrep_arguments = {
+      'rg',
+      '--color=never',
+      '--no-heading',
+      '--with-filename',
+      '--line-number',
+      '--column',
+      '--smart-case',
+      '--no-ignore',
+    },
+    mappings = {
+      i = {
+        ["<esc>"] = require('telescope.actions').close,
       },
+    },
+  },
+  pickers = {
+    live_grep = {
+      additional_args = function()
+        return { "--ignore-case", "--fixed-strings" }
+      end,
     },
   },
 }
 
-require("telescope").load_extension "file_browser"
-
-vim.api.nvim_set_keymap("n", "<space>fe", ":Telescope file_browser<CR>",{ noremap = true })
-
-vim.api.nvim_set_keymap("n", "<space>fc", ":Telescope file_browser path=%:p:h select_buffer=true<CR>",{ noremap = true })
-
--- Map the Telescope current buffer fuzzy find command
-vim.api.nvim_set_keymap('n', '<leader>sw', ':Telescope current_buffer_fuzzy_find<CR>', { noremap = true, silent = true })
+-- Key mappings
+vim.api.nvim_set_keymap("n", "<leader>ff", ":Telescope find_files<cr>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<leader>fg", ":Telescope live_grep<cr>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<C-Tab>", ":Telescope buffers<cr>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<leader>fh", ":Telescope help_tags<cr>", { noremap = true, silent = true })
